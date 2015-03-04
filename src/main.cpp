489f3f868ab4b0be79d1b38bfe17efe9846c7fd9
stryku <stryku2393@o2.pl>
2015-03-04 02:28:23 +0100

    some code clean

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-#include <Fat32Manager.hpp>
-#include <DirectoryEntry.hpp>
 #include <FileHidder.hpp>
 
 
@@ -9,18 +7,6 @@ using namespace std;
 
 int main()
 {
-	Fat32Manager fat32mngr( "fat32example" );
-	DirectoryEntry d;
-
-
-	if( fat32mngr.isValidFat32() )
-		cout << "ta\n";
-	else 
-		cout << "nie\n";
-
-	fat32mngr.printFiles();
-
-	fat32mngr.close();
 
 	FileHidder fileHidder;
 
